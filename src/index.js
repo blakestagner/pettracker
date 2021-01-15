ee221e24b02a287ea7d03843443b8864daf432a0
blakestagner <blakestagner@gmail.com>
2021-01-15 10:41:13 -0800

    pwa edit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render( <App /> , document.getElementById('root')
 );
-
-serviceWorker.register()
+serviceWorkerRegistration.register();
 reportWebVitals();
